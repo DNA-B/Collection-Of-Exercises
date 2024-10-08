4afa4fcb26eb86e269b80b5e42feef01616396fc
변성훈 <sphz3813@gmail.com>
2024-10-08 17:15:27 +0900

    Delete Java_AdvancedOOP/Homework1 directory

--- Java_AdvancedOOP/Homework1/FakePrinter.java
+++
@@ -1,6 +0,0 @@
-public class FakePrinter implements ReceiptPrinter {
-    public void print(StringBuffer buf) {
-        System.out.println("fake Print");
-        System.out.println(buf.toString());
-    }
-}
--- Java_AdvancedOOP/Homework1/HD108ReceiptPrinter.java
+++
@@ -1,6 +0,0 @@
-public class HD108ReceiptPrinter implements ReceiptPrinter {
-    public void print(StringBuffer buf) {
-        System.out.println("Real Print");
-        System.out.println(buf.toString());
-    }
-}
