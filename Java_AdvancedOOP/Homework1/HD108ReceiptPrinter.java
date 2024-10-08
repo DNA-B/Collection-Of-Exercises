b7a8defa6b07dfb76d7d2a39412888b64a4d2827
변성훈 <sphz3813@gmail.com>
2024-10-08 17:16:04 +0900

    Add files via upload

---
+++ Java_AdvancedOOP/Homework1/HD108ReceiptPrinter.java
@@ -0,0 +1,6 @@
+public class HD108ReceiptPrinter implements ReceiptPrinter {
+    public void print(StringBuffer buf) {
+        System.out.println("Real Print");
+        System.out.println(buf.toString());
+    }
+}
