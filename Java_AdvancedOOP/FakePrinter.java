ee6eece6270c914b7b621a62fead1d26e3fad645
변성훈 <sphz3813@gmail.com>
2024-10-08 17:13:22 +0900

    Add files via upload

---
+++ Java_AdvancedOOP/FakePrinter.java
@@ -0,0 +1,6 @@
+public class FakePrinter implements ReceiptPrinter {
+    public void print(StringBuffer buf) {
+        System.out.println("fake Print");
+        System.out.println(buf.toString());
+    }
+}
